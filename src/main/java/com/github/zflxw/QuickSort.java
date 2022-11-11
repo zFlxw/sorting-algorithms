27e41627c9a2aa74c075b6b2dc896bb0283771d3
zFlxw <lxcalflxw@gmail.com>
2022-11-11 20:58:07 +0100

    feat: quick sort implementation

--- src/main/java/com/github/zflxw/QuickSort.java
+++ src/main/java/com/github/zflxw/QuickSort.java
@@ -1,4 +1,47 @@
 package com.github.zflxw;
 
+import com.github.zflxw.util.FileUtils;
+
+import java.util.Arrays;
+
 public class QuickSort {
+    public void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    public int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    public void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int part = partition(array, low, high);
+
+            quickSort(array, low, part - 1);
+            quickSort(array, part + 1, high);
+        }
+    }
+
+    public QuickSort() {
+        int[] numbers = FileUtils.getInput(QuickSort.class);
+        long start = System.currentTimeMillis();
+        quickSort(numbers, 0, numbers.length - 1);
+        System.out.println("Took: " + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    public static void main(String[] args) {
+        new QuickSort();
+    }
 }
---
+++ src/main/java/com/github/zflxw/util/FileUtils.java
@@ -0,0 +1,48 @@
+package com.github.zflxw.util;
+
+import com.github.zflxw.QuickSort;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Just a helper class for IO.
+ */
+public class FileUtils {
+    public static int[] getInput(Class<?> clazz) {
+        File file;
+        try {
+            file = FileUtils.loadResourceFile(clazz, "input.txt");
+            return FileUtils.readFileLines(file).stream().mapToInt(Integer::parseInt).toArray();
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<String> readFileLines(File file) throws IOException {
+        List<String> list = new ArrayList<>();
+        BufferedReader bufferedReader = Files.newBufferedReader(file.toPath());
+
+        bufferedReader.lines().forEach(list::add);
+        bufferedReader.close();
+
+        return list;
+    }
+
+    public static File loadResourceFile(Class<?> clazz, String fileName) throws URISyntaxException {
+        ClassLoader classLoader = clazz.getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+
+        if (resource == null) {
+            throw new IllegalArgumentException("File not found! " + fileName);
+        } else {
+            return new File(resource.toURI());
+        }
+    }
+}
