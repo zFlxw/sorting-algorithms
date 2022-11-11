e5fd82f444b7b886aba93499c6588b6078a4b83b
zFlxw <lxcalflxw@gmail.com>
2022-11-11 21:11:31 +0100

    feat: bubble sort implementation

---
+++ src/main/java/com/github/zflxw/BubbleSort.java
@@ -0,0 +1,29 @@
+package com.github.zflxw;
+
+import com.github.zflxw.util.FileUtils;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        new BubbleSort();
+    }
+
+    public BubbleSort() {
+        int[] numbers = FileUtils.getInput(BubbleSort.class);
+        long start = System.currentTimeMillis();
+        bubbleSort(numbers);
+        System.out.println("Took: " + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    public void bubbleSort(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
